41040b00bf6a68200c91d06c21a0a65fefc3dce7
DNACore <regwize@outlook.com>
2025-05-10 12:03:04 +0800

    Site updated: 2025-05-10 12:03:03

--- js/img-lazyload.js
+++ js/img-lazyload.js
@@ -1,10 +1,1 @@
-/* global Fluid, CONFIG */
-
-(function(window, document) {
-  for (const each of document.querySelectorAll('img[lazyload]')) {
-    Fluid.utils.waitElementVisible(each, function() {
-      each.removeAttribute('srcset');
-      each.removeAttribute('lazyload');
-    }, CONFIG.lazyload.offset_factor);
-  }
-})(window, document);
+(document=>{for(let each of document.querySelectorAll('img[lazyload]'))Fluid.utils.waitElementVisible(each,function(){each.removeAttribute('srcset'),each.removeAttribute('lazyload')},CONFIG.lazyload.offset_factor)})((window,document));
